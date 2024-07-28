b802642da378ae58022e51598a4472ca6ea5415b
damonhuahua <165672990+damonhuahua@users.noreply.github.com>
2024-07-28 20:40:07 +0800

    Add files via upload

--- api/main.py
+++ api/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import google.generativeai as genai
 import json
 import time
-from openai import OpenAI
+# from openai import OpenAI
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware,
@@ -38,29 +38,29 @@ def gemini_chat(data: dict):
         print("gemini_chat:",e)
         return None
 
-@app.post("/openai/chat", tags=["API"], summary="OPENAI")
-def openai_chat(data: dict):
-    prompt = data.get('prompt')
-    api_key= data.get('api_key')
-    history=data.get('history')
-    model_version=data.get('model_version')
-    if history is None:
-        history=[]  
-    try:
-        client =OpenAI(api_key =api_key)
+# @app.post("/openai/chat", tags=["API"], summary="OPENAI")
+# def openai_chat(data: dict):
+#     prompt = data.get('prompt')
+#     api_key= data.get('api_key')
+#     history=data.get('history')
+#     model_version=data.get('model_version')
+#     if history is None:
+#         history=[]  
+#     try:
+#         client =OpenAI(api_key =api_key)
 
-        completion = client.chat.completions.create(
-        model=model_version,
-        messages=[
-            {"role": "user", "content": prompt}
-        ]
-        )
-        text=completion.choices[0].message
-        response = {"content": text}
-        return response
-    except Exception as e:
-        print("openai_chat:",e)
-        return None
+#         completion = client.chat.completions.create(
+#         model=model_version,
+#         messages=[
+#             {"role": "user", "content": prompt}
+#         ]
+#         )
+#         text=completion.choices[0].message
+#         response = {"content": text}
+#         return response
+#     except Exception as e:
+#         print("openai_chat:",e)
+#         return None
     
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1",workers=1)
